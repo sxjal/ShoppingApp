9bdfdbdaf31d59fcec6f7b839898bd43f9aca7ba
sajal <sajalsahu8@gmail.com>
2023-07-16 03:12:42 +0400

    configuring cards

---
+++ lib/Widgets/bottomrow.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+import "package:shoppingapp/model/datamodel.dart";
+
+class BottomCards extends StatelessWidget {
+  const BottomCards({super.key, required this.item});
+
+  final Data item;
+
+  @override
+  Widget build(context) {
+    return const Text("Sajal");
+  }
+}
