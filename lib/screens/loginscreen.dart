13e7d4a2db9694d79c47b524b895dda3a203961c
sajal <sajalsahu8@gmail.com>
2023-07-15 17:44:38 +0400

    finishing touch to login

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -41,6 +41,81 @@ class LoginScreen extends StatelessWidget {
               padding:
                   const EdgeInsets.symmetric(horizontal: 40.0, vertical: 70),
               child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                //child: ,
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 40.0, vertical: 70),
+              child: ClipRect(
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      //   color: const Color.fromARGB(124, 238, 238, 238)
+                      //       .withOpacity(0.2),
+                      // ),
+                      gradient: const LinearGradient(
+                        colors: [
+                          Color.fromRGBO(225, 225, 225, 0.069),
+                          Color.fromRGBO(225, 225, 225, 0.176),
+                          Color.fromRGBO(225, 225, 225, 0.038),
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                      ),
+                    ),
+                    child: const Center(
+                        child: Text(
+                      "Censored",
+                      style: TextStyle(fontSize: 30),
+                    )),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+        /*
+        Stack(
+          children: [
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(loginbg),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(134, 180, 107, 65),
+                    Color.fromARGB(87, 180, 107, 65),
+                    Color.fromARGB(79, 56, 87, 197),
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 40.0, vertical: 70),
+              child: Container(
                 width: double.infinity,
                 height: double.infinity,
                 decoration: const BoxDecoration(
@@ -49,9 +124,9 @@ class LoginScreen extends StatelessWidget {
                   ),
                   gradient: LinearGradient(
                     colors: [
-                      Color.fromRGBO(225, 225, 225, 0.112),
-                      Color.fromRGBO(225, 225, 225, 0.2),
-                      Color.fromRGBO(225, 225, 225, 0.12),
+                      Color.fromRGBO(225, 225, 225, 0.253),
+                      Color.fromRGBO(225, 225, 225, 0.488),
+                      Color.fromRGBO(225, 225, 225, 0.253),
                     ],
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
@@ -66,11 +141,23 @@ class LoginScreen extends StatelessWidget {
                       fit: BoxFit.cover,
                     ),
                   ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 20,
+                    ),
+                    child: Column(
+                      children: [
+                        Text("sajal"),
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ),
           ],
         ),
+        */
       ),
     );
   }
