1fd01a43b5d86ce0de7c7d376e6860f806dff520
sajal <sajalsahu8@gmail.com>
2023-07-15 17:05:50 +0400

    added life to loginscreen

---
+++ lib/constants/images.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final loginbg = "assets/images/Loginscreenbg.png";
--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:shoppingapp/constants/images.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          "Login Screen",
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey,
-          ),
+        child: Stack(
+          children: [
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(loginbg),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromARGB(178, 207, 123, 75),
+                    Color.fromARGB(149, 80, 114, 235),
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
