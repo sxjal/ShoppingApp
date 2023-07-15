f47d54185cf99e16218e088bf5ccd7652f592d92
sajal <sajalsahu8@gmail.com>
2023-07-16 00:42:15 +0400

    second screen

--- lib/Widgets/upperwidgetlogin.dart
+++ lib/Widgets/upperwidgetlogin.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shoppingapp/constants/images.dart';
 
 class TopWidget extends StatelessWidget {
@@ -38,7 +39,8 @@ class TopWidget extends StatelessWidget {
                   borderRadius: BorderRadius.circular(10.0),
                   border: Border.all(
                     width: 1.0,
-                    color: Color.fromARGB(227, 111, 109, 109).withOpacity(0.2),
+                    color: const Color.fromARGB(227, 111, 109, 109)
+                        .withOpacity(0.2),
                   ),
                 ),
               ),
@@ -66,14 +68,13 @@ class TopWidget extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   const SizedBox(
-                    width: 20,
+                    width: 30,
                   ),
-                  const Icon(
-                    Icons.handshake,
-                    size: 28,
+                  const Image(
+                    image: AssetImage(wave),
                   ),
                   const SizedBox(
-                    width: 10,
+                    width: 20,
                   ),
                   Column(
                     mainAxisSize: MainAxisSize.min,
@@ -83,33 +84,91 @@ class TopWidget extends StatelessWidget {
                       Text(
                         "20/12/22",
                         style: Theme.of(context).textTheme.bodySmall!.copyWith(
-                              color: const Color.fromARGB(255, 100, 100, 100),
+                              color: const Color.fromARGB(255, 208, 208, 208),
+                              fontSize: 12,
+                              fontFamily: GoogleFonts.inter().fontFamily,
                             ),
                         textAlign: TextAlign.left,
                       ),
                       Text(
                         "Sajal Sahu",
                         style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                              color: const Color.fromARGB(255, 240, 237, 237),
+                              color: const Color.fromARGB(255, 208, 208, 208),
+                              fontSize: 18,
+                              fontFamily: GoogleFonts.inter().fontFamily,
                             ),
                         textAlign: TextAlign.left,
                       ),
                     ],
                   ),
                   const Spacer(),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(
-                      Icons.recycling_rounded,
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Color.fromARGB(255, 225, 219, 219),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10),
+                      ),
                     ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.delete,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 20,
                   ),
                   const CircleAvatar(
                     radius: 25,
                     backgroundImage: NetworkImage(
-                        "https://nitdelhi.ac.in/alumniportal/assets/team/SAJAL%20SAHU.jpeg"),
+                      "https://nitdelhi.ac.in/alumniportal/assets/team/SAJAL%20SAHU.jpeg",
+                    ),
                   ),
-                  const Spacer(),
+                  const SizedBox(
+                    width: 30,
+                  ),
                 ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 2,
+                ),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.blueGrey,
+                      size: 25,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.tune,
+                      color: Colors.blueGrey,
+                      size: 25,
+                    ),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                      ),
+                    ),
+                    border: UnderlineInputBorder(),
+                    labelText: 'Search favorite Beverages',
+                    labelStyle: TextStyle(
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -1,10 +1,9 @@
 const loginbg = "assets/images/Loginscreenbg.png";
 const texturelogin = "assets/images/Rectangle.png";
 const logo = "assets/images/logo.png";
-
 const mocha = "assets/images/mocha.png";
 const latte = "assets/images/latte.png";
 const espresso = "assets/images/espresso.png";
 const cappuccino = "assets/images/cappuccino.png";
-
 const overhead = "assets/images/overhead.png";
+const wave = "assets/images/wave.png";
